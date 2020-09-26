cfe772114193f645094b6922f8c637f653ff8457
dileepgodithi <dileep.godithi@gmail.com>
2020-09-25 22:29:19 -0500

    Task and Result classes for node communication

---
+++ src/main/java/model/Result.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Result implements Serializable {
+    private Map<String, DocumentData> documentResults;
+
+    Result(){
+        documentResults = new HashMap<>();
+    }
+
+    public void addDocumentData(String document, DocumentData documentData){
+        documentResults.put(document, documentData);
+    }
+
+    public Map<String, DocumentData> getDocumentResults(){
+        return Collections.unmodifiableMap(documentResults);
+    }
+}
