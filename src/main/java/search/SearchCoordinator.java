5e26f272dcb4688a9bc46dc53617c4a1a3f61010
dileepgodithi <dileep.godithi@gmail.com>
2020-09-29 20:19:50 -0500

    Implement OnRequest interface in SearchCoordinator

--- src/main/java/search/SearchCoordinator.java
+++ src/main/java/search/SearchCoordinator.java
@@ -1,6 +1,8 @@
 package search;
 
 import cluster.management.ServiceRegistry;
+import com.google.protobuf.InvalidProtocolBufferException;
+import model.proto.SearchModel;
 import networking.OnRequest;
 import networking.WebClient;
 
@@ -15,12 +17,29 @@ public class SearchCoordinator implements OnRequest {
     }
     @Override
     public byte[] handleRequest(byte[] requestBytes) {
-        return new byte[0];
+        try {
+            SearchModel.Request request = SearchModel.Request.parseFrom(requestBytes);
+            SearchModel.Response response = createResponse(request);
+
+            return response.toByteArray();
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+        return SearchModel.Response.getDefaultInstance().toByteArray();
     }
 
     @Override
     public String getEndPoint() {
         return ENDPOINT;
+    }
+
+    private SearchModel.Response createResponse(SearchModel.Request request){
+        SearchModel.Response.Builder response = SearchModel.Response.newBuilder();
+
+        //Implement building response
+
+
+        return response.build();
     }
 
 
